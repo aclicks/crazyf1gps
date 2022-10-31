fdcc1f7264f884cfeb0caf7c5cc6cd5311aa99df
André Licks <andrelicks@gmail.com>
2022-10-31 10:51:57 -0400

    JS start

---
+++ Index.js
@@ -0,0 +1,5 @@
+let score = 0;
+let questions = [
+    {question: "", rightAnswer: "", wAnswer1: "", wAnswer2: "", wAnswer3: "", wAnswer4: "", tip1: "", tip2: "", tip3: "", tip4: ""}
+];
+let result = "0";
