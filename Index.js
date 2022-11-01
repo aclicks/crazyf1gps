13c9e3c0f9ba11ee683178a76634b0f57d957966
André Licks <andrelicks@gmail.com>
2022-11-01 09:23:02 -0400

    Criação de botão funcionando

--- Index.js
+++ Index.js
@@ -15,6 +15,7 @@ class CrazyGPs {
  ];
     this.result = 0;
     this.round = 0;
+    this.page = 0;
     }
     play () {
         let gameBoard = document.querySelector("#gameBoard");
@@ -26,18 +27,18 @@ class CrazyGPs {
     }
 
     showQuestion () {
-        let question1 = document.querySelector(".question1");
-        let answer1 = document.querySelector(".answer1");
-        let answer2 = document.querySelector(".answer2");
-        let answer3 = document.querySelector(".answer3");
-        let answer4 = document.querySelector(".answer4");
-        let answer5 = document.querySelector(".answer5");
-        question1.innerText  = this.questions[0].questionEasy
-        answer1.innerText = this.questions[0].answers[0];
-        answer2.innerText = this.questions[0].answers[1];
-        answer3.innerText = this.questions[0].answers[2];
-        answer4.innerText = this.questions[0].answers[3];
-        answer5.innerText = this.questions[0].answers[4];
+        let question = document.querySelector(".question1");
+        for(let i = 0; i < 6; i++){
+            if (i === 0){
+                question.innerText = this.questions[0].questionEasy;
+            }
+            else{
+                const button = document.createElement("button");
+                button.className = "answer";
+                button.innerText = this.questions[this.page].answers[i-1];
+                question.appendChild(button);
+            }
+        }
     }
 
     checkAnswer () {
